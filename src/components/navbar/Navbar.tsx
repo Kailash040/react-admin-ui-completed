12ccbb3e641de9bf561c22f77c4f47dd69a37b8a
kailash <kailashrajut0575@gmail.com>
2023-07-16 15:28:15 +0530

    WC-1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
             alt=""
           />
-          <span>Jane</span>
+          <span>Kailash</span>
         </div>
         <img src="/settings.svg" alt="" className="icon" />
       </div>
