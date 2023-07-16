d2f7f7f6eb8eabd1801f810a6eec65576f67d925
kailash <kailashrajut0575@gmail.com>
2023-07-16 14:24:47 +0530

    WC-1

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Kailash</span>
+      <span>©  Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
