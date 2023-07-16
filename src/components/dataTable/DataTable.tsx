22eb31bffad14ca8d28a86919a5b7d1590c48126
kailash <kailashrajut0575@gmail.com>
2023-07-16 15:41:28 +0530

    Wc-1

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -29,10 +29,10 @@ const DataTable = (props: Props) => {
   // //   }
   // // });
 
-  const handleDelete = (id: number) => {
-    //delete the item
-    // mutation.mutate(id)
-  };
+  // const handleDelete = (id: number) => {
+  //   //delete the item
+  //   // mutation.mutate(id)
+  // };
 
   const actionColumn: GridColDef = {
     field: "action",
@@ -44,7 +44,9 @@ const DataTable = (props: Props) => {
           <Link to={`/${props.slug}/${params.row.id}`}>
             <img src="/view.svg" alt="" />
           </Link>
-          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+          <div className="delete" 
+          // onClick={() => handleDelete(params.row.id)}
+          >
             <img src="/delete.svg" alt="" />
           </div>
         </div>
