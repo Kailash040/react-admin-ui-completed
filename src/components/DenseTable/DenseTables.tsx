cb82e0a282c410d053ca8053389b8d87452f0935
kailash <kailashrajut0575@gmail.com>
2023-11-21 21:18:54 +0530

    ui complete

--- src/components/DenseTable/DenseTables.tsx
+++ src/components/DenseTable/DenseTables.tsx
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(() => ({
  
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
